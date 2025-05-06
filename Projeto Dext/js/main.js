40f40f2c7365f10b92e779c992b13d99600e064e
ramossskjk <piresrebelojoaohenrique@gmail.com>
2025-05-06 14:30:21 -0300

    Add files via upload

---
+++ Projeto Dext/js/main.js
@@ -0,0 +1,17 @@
+var menuIcon = document.querySelector('.menu-icon');
+var navList = document.querySelector('.nav-list');
+
+menuIcon.addEventListener('click', () => {
+
+    if (navList.classList.contains('ativo')) {
+        navList.classList.remove('ativo');
+        document.querySelector ('.menu-icon img'),src = img/menuIcon
+        }else{
+            navList.classList.add ('ativo');
+
+        
+    }
+
+}
+
+)
